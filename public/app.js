384b6a9640861e30b06821e3aba3866db37d3399
Farid Hajnal <faridhajnal14@hotmail.com>
2016-06-21 23:28:48 +0200

    Full basic implementation in record time.

--- public/app.js
+++ public/app.js
@@ -29,16 +29,48 @@ app.config(['$routeProvider', function(routeProvider){
 
 }]);
 
-app.controller('MainController', ['$scope', function(scope){
+app.controller('MainController', ['$scope', '$http', function(scope,http){
 
 	scope.message = 'Hola perro';
+
+	http.get('json/videogames.json').success(function(data){
+
+		console.log(data);
+		scope.videogames = data;
+
+	})
 
 
 }]);
 
-app.controller('DetailController', ['$scope', function(scope){
+app.controller('DetailController', ['$scope', '$http', '$routeParams','$filter',
+	function(scope,http,routeParams,filter){
 
-	scope.message = 'Hola perro';
+	var id = routeParams.gameId;
+
+	http.get('json/videogames.json').success(function(data){
+
+		console.log(data);
+		scope.videogame = filter('filter')(data, function(d){
+
+			return d.id == id;
+
+		})[0];
+
+		scope.mainimage = scope.videogame.images[0].name;
+
+		console.log(scope.mainimage);
+
+		scope.numberofimages = scope.videogame.images.length;
+		console.log(scope.videogame.images.length);
+
+	});
+
+	scope.setImage = function(image){
+
+		scope.mainimage = image.name;
+
+	}
 
 
 }]);
